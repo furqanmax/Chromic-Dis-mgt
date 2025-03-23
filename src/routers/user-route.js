0f96cae6c97048d2822627a8245fce2d8b5aaa19
Ashish Kumar Gupta <ashish.gupta3010@gmail.com>
2025-03-23 18:34:52 +0530

    few improvement again

--- src/routers/user-route.js
+++ src/routers/user-route.js
@@ -5,7 +5,11 @@ const {body, param} = require('express-validator');
 
 const router = express.Router();
 
-router.post('/register', [body('email').notEmpty().isEmail(), body('password').notEmpty().isString(), , body('type').notEmpty().in(['patient','doctor'])], register);
+router.post('/register', [
+	body('email').notEmpty().isEmail(), 
+	body('password').notEmpty().isString(), 
+	body('confirm_password').equals(req.body.password),
+ 	body('type').notEmpty().in(['patient','doctor'])], register);
 router.post('/login', [body('email').notEmpty().isEmail(), body('password').notEmpty().isString()], login);
 router.get('/list', [authenticateUser, authorizationUser], getPatentList);
 router.get('/:uid', [param('uid').notEmpty().isString(), authenticateUser, authorizationUser], getUserById);
